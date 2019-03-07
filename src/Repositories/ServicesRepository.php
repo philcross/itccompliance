e9ed695c54e5b3038fe1a8d294652617749b4d42
Phil Cross <p.cross@live.co.uk>
2019-03-07 00:36:48 +0000

    Make repository keep trying to retrieve products on failure

--- src/Repositories/ServicesRepository.php
+++ src/Repositories/ServicesRepository.php
@@ -2,6 +2,9 @@
 
 namespace Philcross\Itc\Repositories;
 
+use Philcross\Itc\Models\Product;
+use Philcross\Itc\Sdk\SdkException;
+use Philcross\Itc\Models\ProductOverview;
 use Philcross\Itc\Adapters\AdapterInterface;
 
 class ServicesRepository
@@ -21,17 +24,50 @@ class ServicesRepository
 
     /**
      * Return an array of all products provided by the service
-     * 
+     *
      * @return array
      */
     public function fetchAllProducts()
     {
         $products = [];
 
-        foreach ($this->adapter->listProducts() as $overview) {
-            $products[] = $this->adapter->fetchProduct($overview->id());
+        foreach ($this->listProducts() as $overview) {
+            $products[] = $this->fetchProduct($overview->id());
         }
 
         return $products;
+    }
+
+    /**
+     * Keep attempting to fetch products from the service. This is a recursive call method.
+     * If an SDK exception is caught, it will recall the method until we get a valid response.
+     *
+     * @return array|ProductOverview[]
+     */
+    private function listProducts()
+    {
+        try {
+            return $this->adapter->listProducts();
+        } catch (SdkException $e) {
+            return $this->listProducts();
+        }
+    }
+
+    /**
+     * Keep attempting to fetch a specific product from the service. This is a recursive call method,
+     * similar to the listProducts method. It will keep trying to retrieve the product if the service
+     * returns an error.
+     *
+     * @param string $id
+     *
+     * @return Product
+     */
+    private function fetchProduct($id)
+    {
+        try {
+            return $this->adapter->fetchProduct($id);
+        } catch (SdkException $e) {
+            return $this->fetchProduct($id);
+        }
     }
 }
--- tests/Repositories/ServicesRepositoryTest.php
+++ tests/Repositories/ServicesRepositoryTest.php
@@ -2,6 +2,8 @@
 
 namespace Philcross\Itc\Tests\Repositories;
 
+use GuzzleHttp\Psr7\Request;
+use Philcross\Itc\Sdk\ClientException;
 use PHPUnit\Framework\TestCase;
 use Philcross\Itc\Models\Product;
 use Philcross\Itc\Models\ProductOverview;
@@ -20,6 +22,70 @@ class ServicesRepositoryTest extends TestCase
         $adapter->shouldReceive('listProducts')->once()->andReturn(ProductOverview::fromResponse($productListArray));
         $adapter->shouldReceive('fetchProduct')->once()->with('combgap')->andReturn(Product::fromResponse('combgap', $productsArray['combgap']));
         $adapter->shouldReceive('fetchProduct')->once()->with('smart')->andReturn(Product::fromResponse('smart', $productsArray['smart']));
+
+        $repository = new ServicesRepository($adapter);
+
+        $products = $repository->fetchAllProducts();
+
+        $this->assertIsArray($products);
+        $this->assertCount(2, $products);
+
+        foreach ($products as $product) {
+            $this->assertInstanceOf(Product::class, $product);
+        }
+    }
+
+    /** @test */
+    public function it_keeps_retrying_to_list_products_upon_failure()
+    {
+        $productListArray = include(__DIR__ . '/../Sdk/responses/list_response.php');
+        $productsArray = include(__DIR__.'/../Sdk/responses/product_response.php');
+
+        $adapter = \Mockery::mock(AdapterInterface::class);
+        $adapter->shouldReceive('listProducts')->andReturnUsing(function () use ($productListArray) {
+            static $initialReturn = true;
+
+            if ($initialReturn) {
+                $initialReturn = false;
+                throw new ClientException('', new Request('GET', 'list'));
+            }
+
+            return ProductOverview::fromResponse($productListArray);
+        });
+        $adapter->shouldReceive('fetchProduct')->once()->with('combgap')->andReturn(Product::fromResponse('combgap', $productsArray['combgap']));
+        $adapter->shouldReceive('fetchProduct')->once()->with('smart')->andReturn(Product::fromResponse('smart', $productsArray['smart']));
+
+        $repository = new ServicesRepository($adapter);
+
+        $products = $repository->fetchAllProducts();
+
+        $this->assertIsArray($products);
+        $this->assertCount(2, $products);
+
+        foreach ($products as $product) {
+            $this->assertInstanceOf(Product::class, $product);
+        }
+    }
+
+    /** @test */
+    public function it_keeps_retrying_to_fetch_products_upon_failure()
+    {
+        $productListArray = include(__DIR__ . '/../Sdk/responses/list_response.php');
+        $productsArray = include(__DIR__.'/../Sdk/responses/product_response.php');
+
+        $adapter = \Mockery::mock(AdapterInterface::class);
+        $adapter->shouldReceive('listProducts')->once()->andReturn(ProductOverview::fromResponse($productListArray));
+        $adapter->shouldReceive('fetchProduct')->once()->with('combgap')->andReturn(Product::fromResponse('combgap', $productsArray['combgap']));
+        $adapter->shouldReceive('fetchProduct')->once()->with('smart')->andReturnUsing(function () use ($productsArray) {
+            static $initialReturn = true;
+
+            if ($initialReturn) {
+                $initialReturn = false;
+                throw new ClientException('', new Request('GET', 'list'));
+            }
+
+            return Product::fromResponse('smart', $productsArray['smart']);
+        });
 
         $repository = new ServicesRepository($adapter);
 
